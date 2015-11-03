09b9ebbd8c8b5ccb96a4bd7fb9e47943e55a8fea
Francis-Jr <jakob.unfried@t-online.de>
2015-11-03 09:59:08 +0100

    Styleguide changes
    Flussdiagramm fehlt noch

--- src/blatt03/Multtable.java
+++ src/blatt03/Multtable.java
@@ -3,33 +3,41 @@ package blatt03;
 import java.util.Scanner;
 
 public class Multtable {
-
-	//TODO Styleguide
+	/**
+	 * Dieses Programm gibt eine Multiplikationstabelle bis zu einer Zahl 
+	 * oberGrenze in die Konsole aus, die vom user abgefragt wird.
+	 */
 	
 	private static Scanner keyboard = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		String c = "";
-		int x = -1;
+		//Input Variabeln mit default-Werten deklarieren
+		String inputString = "";
+		int oberGrenze = -1; //obere Grenze (=Kantenlänge) der Multiplikationstabelle
+		
+		//Frage stellen
 		System.out.println("Bis zu welcher Zahl soll die Multiplikationstabelle aufgestellt werden?");
+		
+		//sooft integer einlesen, bis es das gewünschte Format hat.
 		while(true){
-			c = keyboard.nextLine();
+			inputString = keyboard.nextLine();
 			
-			try {x = Integer.parseInt(c);}
+			try {oberGrenze = Integer.parseInt(inputString);}
 			catch (NumberFormatException e){
 				System.out.println("Ungültige Eingabe (keine Zahl), bitte wiederholen:");
 				continue;
 			}
-			if(x>0) break;
+			if(oberGrenze>0) break; //ACHTUNG: wenn dieses break entfernt wird, ergibt sich eine Endlosschleife!
 			System.out.println("Ungültige Eingabe (Zahl negativ oder null), bitte wiederholen:");
 		}
+		
+		//Input schließen
 		keyboard.close();
 		
-		
-		for (int zeile = 1 ; zeile <= x ; zeile++){
-			for (int spalte = 1 ; spalte <= x ; spalte++){
-				System.out.print(zeile*spalte);
-				System.out.print("\t");
+		//Multiplikationstabelle in die Konsole ausgeben
+		for (int zeile = 1 ; zeile <= oberGrenze ; zeile++){
+			for (int spalte = 1 ; spalte <= oberGrenze ; spalte++){
+				System.out.print(zeile*spalte + "\t");
 			}
 			System.out.println();
 		}
--- src/blatt03/Sternkreuz.java
+++ src/blatt03/Sternkreuz.java
@@ -3,26 +3,46 @@ package blatt03;
 import java.util.Scanner;
 
 public class Sternkreuz {
-
-	//TODO Sytleguide
+	/**
+	 * Dieses Programm fragt eine Kantenlänge vom user ab und zeichnet ein 
+	 * Kreuzförmiges Muster aus Sternchen '*' in die Konsole. 
+	 * Dieser Vorgang wird solange wiederholt, bis ein Kreuz der Kantenlänge
+	 * 2 oder 42 gezeichnet wurde.
+	 */
 	
-	private static int kantenlaenge = 0;
-	private static String sternkreuz = "";
-	private static Scanner keyboard = new Scanner(System.in);
+	private static int kantenlaenge = 0; // wird in input() vom user abgefragt
+	private static String sternkreuz = ""; // wird in draw() beschrieben
+	
+	/**
+	 * Nach dem Zeichnen von Kreuzen dieser speziellen Kantenlängen 
+	 * wird das Programm abgebrochen.
+	 */
+	private static int 	spezielleKantenlaenge1 = 2,
+						spezielleKantenlaenge2 = 42;
 	
 	public static void main(String[] args) {
-		while(kantenlaenge != 2 && kantenlaenge != 42){
+		int anzahlKreuze = 0;
+		while(kantenlaenge !=  spezielleKantenlaenge1 && kantenlaenge != spezielleKantenlaenge2){
 			input();
 			draw();
 			output();
+			if(kantenlaenge == spezielleKantenlaenge1 || kantenlaenge == spezielleKantenlaenge2) {
+				System.out.println(">>Ok, jetzt hab ich aber genug...<<");
+				break;
+			}
+			dialog(anzahlKreuze);
+			anzahlKreuze = anzahlKreuze + 1;
 		}
-		keyboard.close();
 	}
 	
+	/**
+	 * Liest die Kantenlänge aus der Konsole ein.
+	 */
 	private static void input(){
+		Scanner keyboard = new Scanner(System.in);
 		String inputString = "";
 		int inputInt = -1;
-		System.out.println("Geben sie die Kantenlänge ein");
+		System.out.println(">>Geben sie die Kantenlänge ein<<");
 		while(true){
 			inputString = keyboard.nextLine();
 			
@@ -36,8 +56,12 @@ public class Sternkreuz {
 		}
 		
 		kantenlaenge = inputInt;
+		keyboard.close();
 	}
 	
+	/**
+	 * Speichert das Kreuz in den String sternkreuz
+	 */
 	private static void draw(){
 		sternkreuz = "";
 		for(int zeile = 1 ; zeile <= kantenlaenge ; zeile ++){
@@ -56,8 +80,27 @@ public class Sternkreuz {
 		}
 	}
 	
+	/**
+	 * Schriebt das Kreuz aus dem String sternkreuz in die Konsole
+	 */
 	private static void output(){
 		System.out.println(sternkreuz);
+	}
+	
+	/**
+	 * Führt einen kleinen humorvollen Dialog mit dem user durch.
+	 * @param anzahl Anzahl an Kreuzen die vor dem aktuellen schon gezeichnet wurden
+	 */
+	private static void dialog(int anzahl){
+		System.out.println(">>Ich will noch ein Kreuz zeichnen!<<");
+		if(anzahl>4){
+			System.out.println("Vielleicht solltest du es mit der Antwort auf die "
+					+ "Frage aller Fragen versuchen...");
+		}
+		else if(anzahl>2){
+			System.out.println("Tut mir leid, das Programm ist etwas zickig... "
+					+ "vielleicht wartet es auf eine bestimmte Zahl...");
+		}
 	}
 
 }
